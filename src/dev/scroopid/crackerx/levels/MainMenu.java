64554a28958fb817ee3117a3d5d55a8ecedd284e
DevBo <scroopid@hotmail.com>
2013-12-15 18:00:30 -0800

    added new level

---
+++ src/dev/scroopid/crackerx/levels/MainMenu.java
@@ -0,0 +1,15 @@
+package dev.scroopid.crackerx.levels;
+
+import dev.scroopid.crafexEngine.level.Level;
+import dev.scroopid.crafexEngine.ui.UILayer;
+
+public class MainMenu extends Level{
+	
+	public MainMenu(){
+		super();
+		addLayer(new UILayer(){
+			
+		});
+	}
+	
+}
