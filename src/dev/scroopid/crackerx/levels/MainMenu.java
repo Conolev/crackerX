fd44d8c30b7f2efab56c14904dc73ffc036c12cd
DevBo <scroopid@hotmail.com>
2013-12-16 20:27:33 -0800

    added a uilayer to mainmenu with a button that moves when pressed

--- src/dev/scroopid/crackerx/levels/MainMenu.java
+++ src/dev/scroopid/crackerx/levels/MainMenu.java
@@ -1,15 +1,28 @@
 package dev.scroopid.crackerx.levels;
 
+import dev.scroopid.crafexEngine.input.CrafexTouchEvent;
 import dev.scroopid.crafexEngine.level.Level;
+import dev.scroopid.crafexEngine.ui.Button;
 import dev.scroopid.crafexEngine.ui.UILayer;
+import dev.scroopid.crafexEngine.util.floatPoint;
 
 public class MainMenu extends Level{
 	
 	public MainMenu(){
 		super();
-		addLayer(new UILayer(){
+		UILayer layerone = new UILayer();
+		Button test = new Button(null, new floatPoint(200, 200), 1, null){
+			@Override
+			public void whenPressed(CrafexTouchEvent touch) {
+				if(getLocation().getX() == 200){
+					setTargetLocation(500, 500);
+				}else{
+					setTargetLocation(200, 200);
+				}
+			}
 			
-		});
+		};
+		addLayer(layerone);
 	}
 	
 }
